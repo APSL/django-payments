2d123900662f1d77101e798614f8718d6ac133a1
Daniel Sastre <dsastre@apsl.net>
2018-07-04 16:54:59 +0200

    Fix django-sermepa dependency_link name

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ REQUIREMENTS = [
 ]
 
 DEPENDENCY_LINKS = [
-    'git+ssh@github.com:APSL/django-sermepa.git@2.1.0#egg=sermepa-1.1.4',
+    'git+ssh@github.com:APSL/django-sermepa.git@2.1.0#egg=django-sermepa-1.1.4',
 ]
 
 
